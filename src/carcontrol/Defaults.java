a22f3c4a44cb5808d35ee6f14ece1f2dc1452552
Bart Jansen <Bart Jansen@SOSDE49>
2017-06-22 17:07:08 +0200

    Merge origin/master
    Conflicts:
    	src/bart/nl/client/CarGUI.java

--- src/carcontrol/Defaults.java
+++ src/carcontrol/Defaults.java
@@ -117,4 +117,48 @@ public class Defaults {
 
     }
 
+    
+    public static class CarStatus implements Serializable{
+        private boolean forward = false;
+        private boolean backward = false;
+        private boolean left = false;
+        private boolean right = false;
+
+        public boolean isForward() {
+            return forward;
+        }
+
+        public void setForward(boolean forward) {
+            this.forward = forward;
+        }
+
+        public boolean isBackward() {
+            return backward;
+        }
+
+        public void setBackward(boolean backward) {
+            this.backward = backward;
+        }
+
+        public boolean isLeft() {
+            return left;
+        }
+
+        public void setLeft(boolean left) {
+            this.left = left;
+        }
+
+        public boolean isRight() {
+            return right;
+        }
+
+        public void setRight(boolean right) {
+            this.right = right;
+        }
+        
+        
+    }
+    
+    
+    
 }
